e41c8e2022a62e3b015a84245519a95b9d241cbb
yasmin <jeelai.yasmin@gmail.com>
2023-06-30 06:33:34 +0530

    edited-prometheus and grafana

---
+++ 011 - Prometheus-Grafana/install-node-exporter.sh
@@ -0,0 +1,13 @@
+sudo useradd --no-create-home node_exporter
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+
+sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable node-exporter
+sudo systemctl start node-exporter
+sudo systemctl status node-exporter
